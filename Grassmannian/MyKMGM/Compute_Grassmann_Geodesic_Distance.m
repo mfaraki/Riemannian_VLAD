8dd55e268422ee8081a40a01137f9688c303c0fb
Masoud Faraki <masood_faraki@yahoo.com>
2018-07-24 14:22:35 +1000

    Add files via upload

--- Grassmannian/MyKMGM/Compute_Grassmann_Geodesic_Distance.m
+++ Grassmannian/MyKMGM/Compute_Grassmann_Geodesic_Distance.m
@@ -1,29 +1,3 @@
-% Author:
-% - Masoud Faraki (masoud.faraki@nicta.com.au)
-%
-% This file is provided without any warranty of
-% fitness for any purpose. You can redistribute
-% this file and/or modify it under the terms of
-% the GNU General Public License (GPL) as published
-% by the Free Software Foundation, either version 3
-% of the License or (at your option) any later version.
-
-% Please cite the following paper if your are using this code:
-% "More About VLAD: A Leap from Euclidean to Riemannian Manifolds", 
-% Masoud Faraki, Mehrtash Harandi, and Fatih Porikli, 
-% In Proc. IEEE Conference on Computer Vision and Pattern Recognition
-% (CVPR), Boston, USA, 2015.
-
-%  BibTex
-% @inproceedings{faraki2015more,
-%   title={More About VLAD: A Leap from Euclidean to Riemannian Manifolds},
-%   author={Faraki, Masoud and Harandi, Mehrtash T and Porikli, Fatih},
-%   booktitle={In Proc. IEEE Conference on Computer Vision and Pattern Recognition (CVPR)},
-%   pages={4951--4960},
-%   year={2015},
-%   organization={IEEE}
-% }
-
 function outDist = Compute_Grassmann_Geodesic_Distance(Set1,Set2)
 
 simFlag = false;
